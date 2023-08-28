95753e47507deb01c31515afb1a11a7a90b4e00e
Kun Lin <kunlin@usc.edu>
2023-08-28 11:01:19 -0700

    update display

--- assets/javascripts/rss.js
+++ assets/javascripts/rss.js
@@ -17,10 +17,8 @@ fetch(url).then((res) => {
 				let temp = document.importNode(document.querySelector('template').content, true);
 				let i = item.querySelector.bind(item)
 				let t = temp.querySelector.bind(temp)
-				t('h2').textContent = !!i('title') ? i('title').textContent : '-'
+				t('a').textContent = !!i('title') ? i('title').textContent : '-'
 				t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#'
-				t('p').innerHTML = !!i('description') ? i('description').textContent : '-'
-				t('h3').textContent = url.hostname
 				frag.appendChild(temp)
 			})
 		} catch (e) {
