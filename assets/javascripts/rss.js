adf1e35a19f51c3950b6ed31dea1251b92858415
Kun Lin <kunlin@usc.edu>
2023-08-28 11:18:59 -0700

    title

--- assets/javascripts/rss.js
+++ assets/javascripts/rss.js
@@ -11,14 +11,14 @@ fetch(url).then((res) => {
 		try {
 			let doc = DOMPARSER(xmlTxt, "text/xml")
 			let heading = document.createElement('h1')
-			heading.textContent = url.hostname
+			heading.textContent = doc.querySelector('title').textContent
 			frag.appendChild(heading)
 			doc.querySelectorAll('item').forEach((item) => {
 				let temp = document.importNode(document.querySelector('template').content, true);
 				let i = item.querySelector.bind(item)
 				let t = temp.querySelector.bind(temp)
 				t('a').href = !!i('link') ? i('link').textContent : '#'
-				t('a').textContent = t('a').innerHTML = !!i('title') ? i('title').textContent : '-'
+				t('a').textContent = !!i('title') ? i('title').textContent : '-'
 				frag.appendChild(temp)
 			})
 		} catch (e) {
